41a5a504ef4ccb1893fbcae24deb2fbe6b234328
Lisu <415357008@qq.com>
2021-10-15 11:38:55 +0800

    fixed:解决冲突

--- src/components/pages/common/box-container.vue
+++ src/components/pages/common/box-container.vue
@@ -34,13 +34,8 @@ export default {
     },
     title: {
       type: String,
-<<<<<<< HEAD
       default: '组件'
     },
-=======
-      default: ''
-    }
->>>>>>> f655b428323e1958eafad5b7063eaf61d26be897
     /* showOptionHandler: {
       type: Function,
       default: () => {
