fa6ae7e28c33c8a9acf3bc77845583b42b9acc73
Lisu <415357008@qq.com>
2022-02-11 17:42:02 +0800

    update:pie组件导出方式

--- src/components/cell/pie/index.js
+++ src/components/cell/pie/index.js
@@ -3,7 +3,7 @@ import pieStyle2 from "./pie-style-2"
 import pieStyle3 from "./pie-style-3"
 import pieStyle4 from "./pie-style-4"
 
-export default{
+export default {
   components: {
     pieStyle1,
     pieStyle2,
